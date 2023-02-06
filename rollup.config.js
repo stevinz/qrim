707a7e8960e37fa4f7a75da28fddc3ae4c0d97fd
Stephens Nunnally <stevinz@gmail.com>
2023-02-06 11:09:22 -0800

    Updated build

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ function header() {
  * @about       Stylized JavaScript QR Code image generator with UTF8 support.
  * @license     MIT - Copyright (c) 2023 Stephens Nunnally
  * @source      https://github.com/stevinz/qrim
- * @version     v1.0.0
+ * @version     v1.0.1
  * @author      stevinz (module)        https://github.com/stevinz/qrcodejs
  * @author      19z (UTF-8)             https://github.com/19z/qrcodejs-fixUTF8
  * @author      davidshimjs (bundle)    https://github.com/davidshimjs/qrcodejs
